7597851ea80cace393192f450aa2f50514fb70d9
Thomas Perl <m@thp.io>
2013-10-18 01:33:48 +0200

    Fix boot image creation (ramdisk offset)
    Change-Id: Id26f11cd798bcafd5fb43e2ea973854046bab2fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ TARGET_NO_RADIOIMAGE := true
 # Kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x13000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1200000
 
 # Additional libraries
 TARGET_PROVIDES_LIBAUDIO := true
